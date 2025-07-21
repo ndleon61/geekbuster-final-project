86ff00661d8ab012826d7bfd026007a829a33722
Sanchez17140 <sanchez17140@gmail.com>
2025-07-21 23:18:28 +0000

    home layout

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -9,7 +9,37 @@ export const Home = () => {
 
 	return (
 		<div>
-			<h1>If you see this page, you succesfully logged in</h1>
+			<nav class="navbar bg-body-tertiary">
+				<div class="container-fluid">
+					<img src="..." />
+					<form class="d-flex" role="search">
+						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-bookmark"></i></button>
+						<button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-user"></i></button>
+					</form>
+				</div>
+			</nav>
+
+
+			<div class="container text-center">
+				<div class="row row-cols-12">
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col-6">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+					<div class="col">Column</div>
+				</div>
+			</div>
+
 		</div>
+
+
 	);
 }; 
