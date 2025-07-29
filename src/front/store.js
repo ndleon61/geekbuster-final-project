578541ec8b10d9aeb4e65668f6ab444851f6d241
Clay Beach <wcbeach30@gmail.com>
2025-07-29 01:17:54 +0000

    favorites button works, favoritespage works, no images on favorites page for some reason

--- src/front/store.js
+++ src/front/store.js
@@ -1,45 +1,56 @@
-export const initialStore=()=>{
-
+export const initialStore = () => {
   const token = localStorage.getItem("token");
   const user = localStorage.getItem("user");
 
-  return{
+  return {
     token: token || null,
     user: user ? JSON.parse(user) : null,
     favorites: [],
     watchlist: [],
     movies: [],
-    selectedGenre: null
-
-  }
-}
+    selectedGenre: null,
+  };
+};
 
 export default function storeReducer(store, action = {}) {
   switch (action.type) {
-    case 'LOGIN':
+    case "LOGIN":
       localStorage.setItem("token", action.payload.token);
       localStorage.setItem("user", JSON.stringify(action.payload.user));
       return {
         ...store,
         user: action.payload.user,
-        token: action.payload.token
+        token: action.payload.token,
       };
-    case 'LOGOUT':
+    case "LOGOUT":
       localStorage.clear();
       return {
         ...store,
         user: null,
-        token: null
+        token: null,
       };
 
-    case 'set_movies':
+    case "set_movies":
       return { ...store, movies: action.payload };
 
-    case 'set_genre':
+    case "set_genre":
       return { ...store, selectedGenre: action.payload };
-    
-    case 'set_favorites':
-      return {...store, favorites: action.payload}
+
+    case "toggle_favorite": {
+      const { id } = action.payload;
+      const exists = store.favorites.some((fav) => fav.id === id);
+
+      const updatedFavorites = exists
+        ? store.favorites.filter((fav) => !(fav.id === id))
+        : [...store.favorites, { id }];
+
+      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+
+      return {
+        ...store,
+        favorites: updatedFavorites,
+      };
+    }
 
     default:
       throw new Error("Unknown action type");
