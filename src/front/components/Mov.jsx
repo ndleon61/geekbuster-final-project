728c58ea8f22861f81b13dcec75ce19295096e9f
Clay Beach <wcbeach30@gmail.com>
2025-08-04 23:24:28 +0000

    Favorites now work, still have issues with the favorites button not being the correct size

--- src/front/components/Mov.jsx
+++ src/front/components/Mov.jsx
@@ -1,10 +1,9 @@
-import "../styles/Mov.css"; 
+import "../styles/Mov.css";
 import React from "react";
 import MovieRow from "./MovieRow";
 
 
 const apiKey = import.meta.env.VITE_TMDB_KEY;
-console.log("TMDB KEY:", apiKey); 
 
 const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
 
@@ -12,7 +11,7 @@ const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${a
 export default function Mov() {
   return (
     <div>
-      
+
       <MovieRow title="Movies" fetchUrl={popularMoviesUrl} />
     </div>
   );
--- src/front/components/MovieRow.jsx
+++ src/front/components/MovieRow.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import "../styles/MovieRow.css";
@@ -7,6 +8,19 @@ const IMG_BASE = "https://image.tmdb.org/t/p/w500";
 
 const MovieRow = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const { dispatch, store } = useGlobalReducer();
+
+
+  const handleFavoriteToggle = (movie) => {
+    dispatch({
+      type: "toggle_favorite",
+      payload: {
+        id: movie.id,
+        title: movie.title,
+        poster_path: movie.poster_path,
+      },
+    });
+  };
 
   useEffect(() => {
     if (!fetchUrl) return;
@@ -19,25 +33,35 @@ const MovieRow = ({ title, fetchUrl }) => {
 
   return (
     <div className="row">
-  <h2>{title}</h2>
-  <div className="row-posters">
-    {movies
-      .filter((movie) => movie.poster_path)
-      .map((movie) => (
-        <div key={movie.id} className="movie-card">
-          <img
-            className="row-poster"
-            src={`${IMG_BASE}${movie.poster_path}`}
-            alt={movie.title || movie.name}
-          />
-          <h3 className="movie-title">{movie.title || movie.name}</h3>
-          <Link to={`/movie/${movie.id}`} className="details-button">
-            View Details
-          </Link>
-        </div>
-      ))}
-  </div>
-</div>
+      <h2>{title}</h2>
+      <div className="row-posters">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => {
+            const isFavorite = store.favorites.some(
+              (fav) => String(fav.id) === String(movie.id)
+            );
+
+            return <div key={movie.id} className="movie-card">
+              <img
+                className="row-poster"
+                src={`${IMG_BASE}${movie.poster_path}`}
+                alt={movie.title || movie.name}
+              />
+              <h3 className="movie-title">{movie.title || movie.name}</h3>
+              <Link to={`/movie/${movie.id}`} className="details-button">
+                View Details
+              </Link>
+              <button
+                className="btn btn-sm btn-outline-warning"
+                onClick={() => handleFavoriteToggle(movie)}
+              >
+                <i className={`fa ${isFavorite ? "fa-solid fa-star" : "fa-regular fa-star"}`} />
+              </button>
+            </div>
+          })}
+      </div>
+    </div>
   );
 };
 
